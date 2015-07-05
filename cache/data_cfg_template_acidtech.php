ae027a0ae98247a4df63246fefcb7b57ad4bbb97
glacials <qhiiyr@gmail.com>
2015-07-04 21:10:04 -0700

    Get cache out of the repo

--- cache/data_cfg_template_acidtech.php
+++
@@ -1,4 +0,0 @@
-<?php exit; ?>
-1446351511
-141
-a:4:{s:4:"name";s:8:"AcidTech";s:9:"copyright";s:31:"&copy; 2007 Vjacheslav Trushkin";s:7:"version";s:5:"1.2.0";s:8:"filetime";i:1347166963;}
--- cache/tpl_acidtech_ca_config.html.php
+++
@@ -1,5 +0,0 @@
-<?php if (!defined('IN_PHPBB')) exit; if ($this->_tpldata['DEFINE']['.']['CA_COMMENTS']) {  ?>
-
-$CA_SPACING - cellspacing for all tables
-$CA_WIDTH - page width. examples: '100%', '800'
-<?php } $this->_tpldata['DEFINE']['.']['CA_WIDTH'] = '80%'; $this->_tpldata['DEFINE']['.']['CA_SPACING'] = '0'; $this->_tpldata['DEFINE']['.']['CA_CAP_START'] = '<caption><div class="cap-left"><div class="cap-right">&nbsp;'; $this->_tpldata['DEFINE']['.']['CA_CAP_END'] = '&nbsp;</div></div></caption>'; $this->_tpldata['DEFINE']['.']['CA_BLOCK_START'] = ''; $this->_tpldata['DEFINE']['.']['CA_BLOCK_END'] = ''; ?>
